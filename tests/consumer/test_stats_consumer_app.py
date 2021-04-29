f0550c940528ab2990072ea1e0a8a70662b45f86
Sukhdeep Singh <sdsingh.developer@gmail.com>
2021-04-29 20:33:32 +0200

    UT failures fix

--- tests/consumer/test_stats_consumer_app.py
+++ tests/consumer/test_stats_consumer_app.py
@@ -10,17 +10,18 @@ from src.consumer.stats_consumer_app import StatsConsumerApp
 def stats_consumer_app(db_mock, consumer_mock, cfg_read, mocker):
     return consumer_mock, StatsConsumerApp(cfg_read, db_mock)
 
+
 class TestStatsConsumerApp:
-    @pytest.mark.asyncio
-    async def test_consumer_start(self, stats_consumer_app, mocker):
-        consumer, app = stats_consumer_app
-
-        await app.run()
-        consumer.return_value.start.assert_called_once_with()
-
     @pytest.mark.asyncio
     async def test_consumer_stop(self, stats_consumer_app, mocker):
         consumer, app = stats_consumer_app
 
         app.stop()
         consumer.return_value.stop.assert_called_once()
+        
+    @pytest.mark.asyncio
+    async def test_consumer_start(self, stats_consumer_app, mocker):
+        consumer, app = stats_consumer_app
+
+        await app.run()
+        consumer.return_value.start.assert_called_once_with()
